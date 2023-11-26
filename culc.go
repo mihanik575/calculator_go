520c32809684abfee2ecb6965504ca6d8b4be2c1
mihanik575 <m9854737384@gmail.com>
2023-11-26 16:20:31 +0300

    первые пробники

---
+++ culc.go
@@ -0,0 +1,35 @@
+package main
+
+for {
+fmt.Print("Введите выражение (в формате 2+5) (или 'exit' для выхода): ")
+fmt.Scanln(&input)
+
+if strings.ToLower(input) == "exit" {
+fmt.Println("Программа завершена")
+break
+}
+func plus(x, y int) int {
+	return x + y
+}
+func mines(x, y int) int {
+	return x - y
+}
+
+func minus(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y float64) float64 {
+	return x / y
+}
+
+func main() {
+	fmt.Println(plus(42, 13))
+	fmt.Println(mines(42, 13))
+	fmt.Println(divide(42, 13))
+	fmt.Println(multiply(42, 13))
+}
